ae4c42af3c864694e60cfee824096622678ca42a
Snehashish pramanick <100946099+VVNEOM@users.noreply.github.com>
2023-08-11 10:42:36 +0530

    Add files via upload

---
+++ src/library/management/system/pkg2/logout.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library.management.system.pkg2;
+
+/**
+ *
+ * @author sneha
+ */
+public class logout {
+
+    public logout() {
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
